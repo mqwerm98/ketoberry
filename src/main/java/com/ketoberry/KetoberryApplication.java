f6d8c086b966c2f8a1fe09bde704dfa7f393f00c
현진집컴 <mqwerm98@naver.com>
2020-09-02 21:06:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/ketoberry/KetoberryApplication.java
@@ -0,0 +1,13 @@
+package com.ketoberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetoberryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KetoberryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ketoberry/KetoberryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketoberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetoberryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
